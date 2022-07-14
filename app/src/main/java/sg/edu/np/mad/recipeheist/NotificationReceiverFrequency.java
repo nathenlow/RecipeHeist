38aa02637a933182167eb9abca49d80814ab180c
punchyface <s10222101@connect.np.edu.sg>
2022-07-14 17:47:16 +0800

    fix cancel button for notification

--- app/src/main/java/sg/edu/np/mad/recipeheist/NotificationReceiverFrequency.java
+++ app/src/main/java/sg/edu/np/mad/recipeheist/NotificationReceiverFrequency.java
@@ -3,12 +3,10 @@ package sg.edu.np.mad.recipeheist;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
-public class NotificationReceiverDelete extends BroadcastReceiver {
+public class NotificationReceiverFrequency extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Start Services
-        context.startService(new Intent(context, UpdateService.class).setAction("STOP_ACTION"));
+        //context.startService(new Intent(context, UpdateService.class).setAction("STOP_ACTION"));
     }
 }
