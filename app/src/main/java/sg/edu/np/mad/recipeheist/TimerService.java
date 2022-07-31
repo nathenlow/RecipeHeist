7506e71667c40b5b9a88f92a44ad9df0a0db6f7d
punchyface <s10222101@connect.np.edu.sg>
2022-08-01 00:24:18 +0800

    fix TimerService

--- app/src/main/java/sg/edu/np/mad/recipeheist/TimerService.java
+++ app/src/main/java/sg/edu/np/mad/recipeheist/TimerService.java
@@ -7,6 +7,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Build;
+import android.os.Handler;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
@@ -36,12 +37,35 @@ public class TimerService extends Service {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void run() {
-                // reduce time
-                timerRemaining[0]--;
-                // display it on notification
-                notificationUpdate(timerRemaining[0]);
-                if (timerRemaining[0] <= 0){
-                    timer.cancel();
+                //if left 3 second --> sound notification
+                if (timerRemaining[0] <= 4){
+                    //cancel when time is up
+                    if (timerRemaining[0] <= -1) {
+                        timer.cancel();
+                    }
+                    else{
+                        Intent notificationIntent = new Intent(getApplicationContext(), CountdownTimerActivity.class);
+                        final PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, notificationIntent, 0);
+
+                        final Notification[] notifications = {new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                                .setContentTitle("Timer: "+ timerRemaining[0] + "s")
+                                .setOnlyAlertOnce(false)
+                                .setSmallIcon(R.drawable.ic_baseline_timer_24)
+                                .setContentIntent(pendingIntent)
+                                .build()
+                        };
+
+                        startForeground(2, notifications[0]);
+
+                        NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "Recipe Heist Timer", NotificationManager.IMPORTANCE_DEFAULT);
+                        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+                        notificationManager.createNotificationChannel(notificationChannel);
+                    }
+                    // reduce time
+                    timerRemaining[0]--;
+                }
+                else {
+                    notificationUpdate(timerRemaining[0]);
                 }
             }
         }, 0, 1000);
@@ -58,14 +82,14 @@ public class TimerService extends Service {
             final PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
 
             final Notification[] notifications = {new NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setContentTitle("Recipe Heist Timer")
-                    .setContentText("Timer: " + timeLeft.toString())
-                    .setSmallIcon(R.mipmap.ic_launcher_foreground)
+                    .setContentTitle("Timer: "+ timeLeft.toString() + "s")
+                    .setOnlyAlertOnce(true)
+                    .setSmallIcon(R.drawable.ic_baseline_timer_24)
                     .setContentIntent(pendingIntent)
                     .build()
             };
 
-            startForeground(1, notifications[0]);
+            startForeground(2, notifications[0]);
 
             NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, "Recipe Heist Timer", NotificationManager.IMPORTANCE_LOW);
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
